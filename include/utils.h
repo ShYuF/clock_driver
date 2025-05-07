20b339b2143d893104ff0ec4613cc9fc89b4328a
雨相羽 <1529403937@qq.com>
2025-05-07 16:15:22 +0800

    v1.1

--- include/utils.h
+++ include/utils.h
@@ -5,50 +5,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <pthread.h>
 
 #ifdef __linux__
     #define PLATFORM_LINUX
 
-    #include <sys/ioctl.h>
-    #include <linux/types.h>
-
-    #include <linux/kernel.h>
-    #include <linux/module.h>
+    #ifndef __KERNEL__
+        #include <unistd.h>
+        #include <fcntl.h>
+        #include <pthread.h>
+        #include <sys/ioctl.h>
+        #include <sys/mman.h>
+        #include <sys/io.h>
+    #endif
 
     #ifdef __KERNEL__
         #include <linux/init.h>
-
+        #include <linux/kernel.h>
+        #include <linux/module.h>
         #include <linux/fs.h>
-        #include <linux/device.h> 
-
+        #include <linux/device.h>
         #include <linux/cdev.h>
-
         #include <linux/slab.h>
-        #include <asm/uaccess.h>
         #include <linux/uaccess.h>
-
-        # include <asm/io.h>
-        #include <linux/io.h>
         #include <linux/ioport.h>
-
         #include <linux/interrupt.h>
-
         #include <linux/time.h>
         #include <linux/timer.h>
         #include <linux/jiffies.h>
-
         #include <linux/isa.h>
+        #include <asm/io.h>
     #endif
 
-    #ifdef __MODULE__
-
-    #endif
 #elif defined(_WIN32) || defined(_WIN64)
     #define PLATFORM_WINDOWS
-
     #include <windows.h>
 #else
     #define PLATFORM_UNKNOWN
